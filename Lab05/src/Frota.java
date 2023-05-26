6857d0710a14ffa52eb5ee91e1b311e5cfa2ac31
Pedro Borges <p260628@dac.unicamp.br>
2023-05-26 19:09:38 -0300

    Todos os métodos implementados na classe Frota

--- Lab05/src/Frota.java
+++ Lab05/src/Frota.java
@@ -23,6 +23,36 @@ public class Frota {
         this.listaVeiculos = listaVeiculos;
     }
 
+    public boolean addVeiculo(Veiculo veiculo, Frota frota){
+        for (Veiculo veiculo1 : frota.getListaVeiculos()){
+            if (veiculo.getPlaca().equals(veiculo1.getPlaca())){
+                System.out.println("Esse veículo já está nesta frota");
+                return false;
+            }
+        }
+        frota.getListaVeiculos().add(veiculo);
+        return true;
+    }
+
+    public boolean removeVeiculo(Veiculo veiculo, Frota frota){
+        int tamanho = frota.getListaVeiculos().size();
+        if(tamanho == 0){
+            System.out.println("Essa frota ainda não tem veículos");
+            return false;
+        }
+
+        for(Veiculo veiculo2 : frota.getListaVeiculos()){
+            if(veiculo.getPlaca().equals(veiculo2.getPlaca())){
+                System.out.println("Veículo Removido");
+                listaVeiculos.remove(veiculo2);
+                return true;
+            }
+        }
+
+            System.out.println("Erro! Esse veículo não se encontra nessa frota");
+            return false;
+    }
+
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Frota [code=").append(code).append(", listaVeiculos=").append(listaVeiculos).append("]");
