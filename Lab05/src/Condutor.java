123088f19ccc504f5a56e9a46b830be40dd6a842
Pedro Borges <p260628@dac.unicamp.br>
2023-05-26 17:53:47 -0300

    Adicionando classes e mundando as existentes

---
+++ Lab05/src/Condutor.java
@@ -0,0 +1,59 @@
+public class Condutor {
+    private final String cpf;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+    private Date dataNasc;
+    private List <Sinistro> listaSinistros;
+
+    public Condutor(String cpf, String nome, String telefone, String email, String endereco, Date dataNasc, List <Sinistro> listaSinistros){
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.dataNasc = dataNasc;
+        this.email = email;
+        this.endereco = endereco;
+        this.listaSinistros = listaSinistros;
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public List<Sinistro> getListaSinistros() {
+        return listaSinistros;
+    }
+    public void setListaSinistros(List<Sinistro> listaSinistros) {
+        this.listaSinistros = listaSinistros;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
